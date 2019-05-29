7da95616ec1e4b55c550b468578d6321adc8517d
cristina <yosola@gmail.com>
2019-05-29 16:45:19 -0700

    Style changes

--- app/src/main/java/co/yosola/nahuatl/NumbersFragment.java
+++ app/src/main/java/co/yosola/nahuatl/NumbersFragment.java
@@ -23,19 +23,19 @@ public class NumbersFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.activity_numbers, container, false);
         ArrayList<Word> wordsOfNumbers = new ArrayList<Word>();
 
-        wordsOfNumbers.add(new Word("One", "ce", R.drawable.number_one_nahuatl));
-        wordsOfNumbers.add(new Word("Two", "ome", R.drawable.number_two_nahuatl));
-        wordsOfNumbers.add(new Word("Three", "yei", R.drawable.number_three_nahuatl));
-        wordsOfNumbers.add(new Word("Four", "nahui", R.drawable.number_four_nahuatl));
-        wordsOfNumbers.add(new Word("Five", "macuilli", R.drawable.number_five_nahuatl));
-        wordsOfNumbers.add(new Word("Six", "chicuace", R.drawable.number_six_nahuatl));
-        wordsOfNumbers.add(new Word("Seven", "chicome", R.drawable.number_seven_nahuatl));
-        wordsOfNumbers.add(new Word("Eight", "chicuei", R.drawable.number_eight_nahuatl));
-        wordsOfNumbers.add(new Word("Nine", "chiconahui", R.drawable.number_nine_nahuatl));
-        wordsOfNumbers.add(new Word("Ten", "mahtlactli", R.drawable.number_ten_nahuatl));
+        wordsOfNumbers.add(new Word("One", "ce", R.drawable.number_one_2));
+        wordsOfNumbers.add(new Word("Two", "ome", R.drawable.number_two));
+        wordsOfNumbers.add(new Word("Three", "yei", R.drawable.number_three));
+        wordsOfNumbers.add(new Word("Four", "nahui", R.drawable.number_four));
+        wordsOfNumbers.add(new Word("Five", "macuilli", R.drawable.number_five));
+        wordsOfNumbers.add(new Word("Six", "chicuace", R.drawable.number_six));
+        wordsOfNumbers.add(new Word("Seven", "chicome", R.drawable.number_seven));
+        wordsOfNumbers.add(new Word("Eight", "chicuei", R.drawable.number_eight));
+        wordsOfNumbers.add(new Word("Nine", "chiconahui", R.drawable.number_nine));
+        wordsOfNumbers.add(new Word("Ten", "mahtlactli", R.drawable.number_ten));
 
 
-        WordAdapter adapter = new WordAdapter(getActivity(), wordsOfNumbers, R.color.colorCardBackground);
+        WordAdapter adapter = new WordAdapter(getActivity(), wordsOfNumbers, R.color.colorPrimaryLight);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
